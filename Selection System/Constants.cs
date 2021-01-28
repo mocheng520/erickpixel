e3231f1c41bde4c9ff025951902c230613b5f5e0
Erick Pixel <61104009+erickluiss@users.noreply.github.com>
2021-01-28 18:44:45 -0300

    Update Constants.cs

--- Selection System/Constants.cs
+++ Selection System/Constants.cs
@@ -2,10 +2,7 @@
 {
     internal static class Constants
     {
-        internal static readonly float maxRayTravelDistance = int.MaxValue;
-        internal const float _minAcceptableVertexDistance = 0.05f;
-        internal const float _ninetyDegreesRotation = 90f;
-        internal const float _rayBlockerHeight = -100f;
-        internal readonly static int _selectionBoxLayer = 1 << References._rayBlockerCollider.gameObject.layer;
+        internal const float MIN_ACCEPTABLE_VERTEX_DISTANCE = 0.05f;
+        internal const float NINETY_DEG_ROTATION = 90f;
     }
 }
